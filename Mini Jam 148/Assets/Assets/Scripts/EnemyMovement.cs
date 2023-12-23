eee48679573154bdd877561ebbeef0645aaf0124
kimizx3 <zhang.ziyang1@northeastern.edu>
2023-12-22 21:31:22 -0500

    Scene Create

---
+++ Mini Jam 148/Assets/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float agroRange = 5f;
+    public float movementSpeed = 2f;
+    public Rigidbody2D rb;
+    public Transform Target;
+    private Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        float distToPlayer = Vector2.Distance(transform.position, Target.position);
+
+        if (distToPlayer < agroRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasePlayer();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        if (Target)
+        {
+            Vector3 direction = (Target.position - transform.position).normalized;
+            moveDirection = direction;
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * movementSpeed;
+        }
+    }
+
+    private void StopChasePlayer()
+    {
+        rb.velocity = new Vector2(0f, 0f);
+    }
+}
